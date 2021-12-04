92c7c82253d276ceca30590f59470ba8db4ac0ce
m.koras <m.koras@campus.tu-berlin.de>
2021-12-04 18:21:51 +0100

    server and client

---
+++ client-mongoDB/src/main/java/DbConnection.java
@@ -0,0 +1,27 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
+public class DbConnection {
+    public static void main() {
+        try {
+
+
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+            MongoDatabase db = mongoClient.getDatabase("benchmark");
+
+            MongoCollection<Document> table = db.getCollection("student");
+            Document document = new Document("name", "Michael Kora");
+            document.append("id", 123456);
+            table.insertOne(document);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
