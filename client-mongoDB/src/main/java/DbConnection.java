4350092c40029d999046d1c93f9fb4a1f6b0cf10
m.koras <m.koras@campus.tu-berlin.de>
2021-12-11 12:42:48 +0100

    test

--- client-mongoDB/src/main/java/DbConnection.java
+++ client-mongoDB/src/main/java/DbConnection.java
@@ -10,27 +10,25 @@ import java.util.Date;
 public class DbConnection {
     public static void main(String[] args) {
         try {
-            System.out.println(11111);
-
-
             MongoClient mongoClient = new MongoClient("localhost", 27017);
-            System.out.println(22222);
             MongoDatabase db = mongoClient.getDatabase("benchmark");
-            System.out.println(33333);
             MongoCollection<Document> table = db.getCollection("student");
-            System.out.println(44444);
+
             DataWriter dataWriter = new DataWriter("./csvFile/output.csv");
 
-            System.out.println(5555555);
             for (int i = 0; i < 10; i++) {
                 System.out.println(i);
                 Document document = new Document("name", "Michael Kora");
                 document.append("id", 123456);
+                System.out.println(5555555);
 
                 SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-                Date start = new Date(System.currentTimeMillis());
+                System.out.println(22222);
 
-                //write in dabase
+                Date start = new Date(System.currentTimeMillis());
+                System.out.println(11111);
+
+                //write in database
                 table.insertOne(document);
 
                 Date end = new Date(System.currentTimeMillis());
