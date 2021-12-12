72bc1ef21a47e452fd7df7d9e99ec43e5416db66
m.koras <m.koras@campus.tu-berlin.de>
2021-12-12 18:13:34 +0100

    test csv writing

--- client-mongoDB/src/main/java/DbConnection.java
+++ client-mongoDB/src/main/java/DbConnection.java
@@ -3,13 +3,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.io.FileWriter;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-
-import static java.lang.Thread.sleep;
 
 
 public class DbConnection {
@@ -19,6 +14,7 @@ public class DbConnection {
             MongoDatabase db = mongoClient.getDatabase("benchmark");
             MongoCollection<Document> table = db.getCollection("student");
 
+            System.out.print("Ananas");
             DataWriter dataWriter = new DataWriter("output.csv");
 
             for (int i = 0; i < 10; i++) {
@@ -26,6 +22,7 @@ public class DbConnection {
                 // data to be inserted
                 Document document = new Document("name", "Michael Kora");
                 document.append("id", 123456);
+                System.out.print("Apfel");
 
                 SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
