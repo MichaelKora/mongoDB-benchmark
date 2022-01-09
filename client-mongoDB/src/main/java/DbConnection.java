f6d79dc9ca64b040b1d5c79b4d87b6c56b5ba761
michaelkora <michael.koras0@gmail.com>
2022-01-09 18:53:34 +0100

    springBoot 2nd try

--- client-mongoDB/src/main/java/DbConnection.java
+++ client-mongoDB/src/main/java/DbConnection.java
@@ -10,11 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SprinApplication;
 
 @SpringBootApplication
 public class DbConnection {
