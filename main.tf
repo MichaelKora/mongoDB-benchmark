8d475f009a16f5ff89b535a2fbb5bee148e97f7c
Michael Kora <80787590+MichaelKora@users.noreply.github.com>
2022-02-17 17:29:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,33 +44,33 @@ resource "google_compute_instance" "default1" {
 
 
 // 3 client  Compute Engine instances
-//resource "google_compute_instance" "default" {
-// count = 0 
-// name         = "client-vm-${count.index}"
-//
-// machine_type = "e2-standard-2"
-// zone         = var.instance_tz[count.index]
-//
-//metadata = {
-//   ssh-keys = "michaelkora:${file("~/.ssh/id_rsa.pub")}"
-// }
-// boot_disk {
-//   initialize_params {
-//     image = "ubuntu-os-cloud/ubuntu-1804-lts"
-//   }
-// }
-//
+resource "google_compute_instance" "default" {
+ count = 0 
+ name         = "client-vm-${count.index}"
+
+ machine_type = "e2-standard-2"
+ zone         = var.instance_tz[count.index]
+
+metadata = {
+   ssh-keys = "michaelkora:${file("~/.ssh/id_rsa.pub")}"
+ }
+ boot_disk {
+   initialize_params {
+     image = "ubuntu-os-cloud/ubuntu-1804-lts"
+   }
+ }
+
 //// Make sure flask is installed on all new instances for later steps
-// metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
-//
-// network_interface {
-//   network = "default"
-//
-//   access_config {
-//     // Include this section to give the VM an external ip address
-//   }
-// }
-//}
+ metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
 //
 resource "google_compute_firewall" "ssh-rule" {
   name    = "mdb-ssh1"
@@ -89,6 +89,6 @@ resource "google_compute_firewall" "ssh-rule" {
   target_tags   = ["mdb-server"]
 }
 //// A variable for extracting the external IP address of the instance
-//output "ip" {
-// value = google_compute_instance.default[0].network_interface.0.access_config.0.nat_ip
-//}
+output "ip" {
+ value = google_compute_instance.default[0].network_interface.0.access_config.0.nat_ip
+}
